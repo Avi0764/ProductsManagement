ad00216a5f1152dd3b69f8616826eefcdc892bc5
Avinash Pandit Rathod <96137627+Avi0764@users.noreply.github.com>
2024-02-29 00:55:23 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string? ProductName { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagement.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> CategoryProduct { get; set;}
+    }
+}
